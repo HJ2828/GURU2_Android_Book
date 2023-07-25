aa0e5df1040cf17459dc9bb134ae0f1680cc2d8f
sj011105 <sj011105@swu.ac.kr>
2023-07-25 11:53:58 +0900

    design added

---
+++ app/src/main/java/com/example/guru2_book/MainActivity15.kt
@@ -0,0 +1,11 @@
+package com.example.guru2_book
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity15 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main15)
+    }
+}
