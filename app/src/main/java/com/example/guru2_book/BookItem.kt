a8c079e0a17dfb3dfa25a9b181c2be21335784ea
sychoi <seoyeon127@swu.ac.kr>
2023-07-27 11:39:49 +0900

    책 검색 & 스캔 xml 추가

---
+++ app/src/main/java/com/example/guru2_book/BookItem.kt
@@ -0,0 +1,10 @@
+package com.example.guru2_book
+
+data class BookItem(
+    val title: String,
+    val author: String,
+    val publisher: String,
+    val pubdate: String,
+    val coverImageUrl: String,
+    val description: String
+)
--- app/src/main/java/com/example/guru2_book/BookSearchFragment.kt
+++ app/src/main/java/com/example/guru2_book/BookSearchFragment.kt
@@ -1,15 +1,18 @@
 package com.example.guru2_book
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.guru2_book.BookAdapter
+import com.example.guru2_book.BookItem
+import kotlinx.coroutines.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -17,15 +20,16 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BookSearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var recyclerViewBooks: RecyclerView
+    private lateinit var edtSearchBook: EditText
+    private lateinit var bookAdapter: BookAdapter
+    private var bookList = emptyList<BookItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -51,8 +55,7 @@ class BookSearchFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             BookSearchFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
